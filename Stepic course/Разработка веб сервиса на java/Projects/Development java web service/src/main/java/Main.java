caa3fee122d15c5063dd1097f2ce89b243c2d6d1
b4w <dzirtdurden@gmail.com>
2015-12-16 00:39:42 +0300

    Added new projects for Stepic Java Service

---
+++ Stepic course/Разработка веб сервиса на java/Projects/Development java web service/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.eclipse.jetty.server.Server;
+
+public class Main {
+    private final static int HTTP_PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
